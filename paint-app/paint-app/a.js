559e437f4dc61143b8de0457509f3851a034343a
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-06-02 11:34:28 +0800

    Add files via upload

---
+++ paint-app/paint-app/a.js
@@ -0,0 +1,52 @@
+var canvasWidth = screen.width;
+var canvasHeight = screen.height;
+var happy = "DIE";
+var optionsMenuShown = false;
+var thickness = document.getElementById('thickness-slider').value;
+var colourAlpha = document.getElementById('colour-alpha-slider').value;
+var lineColour = document.getElementById('colour-picker').value;
+var tool = "pen";
+var vertices = [];
+
+console.log(colourAlpha);
+
+function updateThicknessValue(value) {
+    thickness = value;
+}
+
+function updateColoursValue(value) {
+    lineColour = value;
+    alert(lineColour.value);
+}
+
+function updateAlphaValue(value) {
+    colourAlpha = value;
+}
+
+function showOptionsMenu(value) {
+    var optionsMenu = document.getElementById('options');
+    var showOptionsMenuButton = document.getElementById('showOptionsMenuButton');
+    value ? optionsMenu.style.display = 'none' : value == null ? optionsMenu.style.display = optionsMenuShown ? 'none' : 'block' : optionsMenu.style.display = 'block';
+    showOptionsMenuButton.style.display = 'none';
+    optionsMenuShown = !optionsMenuShown;
+}
+
+function setTool(toolIn) {
+    tool = toolIn;
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.key == 'b') {
+        tool = "line";
+        alert("line"); 
+    }
+	else if (event.key == 'p') {
+		tool = "pen";
+    }
+    else if (event.key == 's') {
+        tool = "shape";
+    }
+    if (event.key == 'm') {
+        showOptionsMenu(!optionsMenuShown);
+    }
+});
---
+++ paint-app/paint-app/sketch.js
@@ -0,0 +1,47 @@
+var start;
+var end;
+
+function setup() {
+  canvas = createCanvas(canvasWidth, canvasHeight);
+  background(255);
+  noStroke();
+  
+}
+
+function mouseDragged() {
+	if (tool == "pen") {
+	  var strokeColour = color(lineColour);
+	  stroke(red(strokeColour), green(strokeColour), blue(strokeColour), colourAlpha);
+	  strokeWeight(thickness);
+	  
+	  line(pmouseX, pmouseY, mouseX, mouseY);
+	}
+	else if (tool == "shape") {
+		vertices.push(createVector(mouseX, mouseY));
+		beginShape();
+		for (var i = 0; i < vertices.length; i++) {
+			vertex(vertices[i].x, vertices[i].y);
+		}
+		endShape();
+	}
+	
+}
+
+function mousePressed() {
+	if (tool == "line") {
+    	start = createVector(mouseX, mouseY);
+		print('started');
+	}
+	else {
+		
+	}
+}
+function mouseReleased() {
+	if (tool == "line") {
+    end = createVector(mouseX, mouseY);
+
+    line(start.x, start.y, end.x, end.y);
+
+  }
+
+}
