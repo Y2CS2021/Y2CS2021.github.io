074edb3dbd74181fbe216c2b408f1b5747f0a2fc
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-06-02 15:58:40 +0800

    Add files via upload

--- paint-app/a.js
+++ paint-app/a.js
@@ -59,9 +59,7 @@ function updateAlphaValue(value) {
 
 function showOptionsMenu(value) {
     var optionsMenu = document.getElementById('options');
-    var showOptionsMenuButton = document.getElementById('showOptionsMenuButton');
     value ? optionsMenu.style.display = 'none' : value == null ? optionsMenu.style.display = optionsMenuShown ? 'none' : 'block' : optionsMenu.style.display = 'block';
-    showOptionsMenuButton.style.display = 'none';
     optionsMenuShown = !optionsMenuShown;
 }
 
