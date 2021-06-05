b92ff5e2ab9b7907970d6e0b1dbb3d841325c44a
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-06-05 15:11:05 +0800

    Add files via upload

---
+++ batch-logo/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  textAlign(CENTER);
+}
+
+function draw() {
+  background(220);
+  ellipse(200, 200, 100, 100);
+  text("Raffles Institution", 200, 200);
+}
